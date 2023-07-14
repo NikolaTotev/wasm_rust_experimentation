cd6c33a723a029402eb1390002fa600bd1824874
NikolaTotev <nikola.totev@ocado.com>
2023-07-14 23:43:14 +0300

    feat Added js

--- wasm_tutorial/src/main.rs
+++ wasm_tutorial/src/main.rs
@@ -1,15 +1,29 @@
 fn main() {
-    for delta_row in [25 - 1, 0, 1].iter().cloned() {
-        for delta_col in [20 - 1, 0, 1].iter().cloned(){
-            //println!("{delta_col} {delta_row}");
+    println!("For 0,0: ");
+    live_neighbor_count(0,0);
+    println!();
+
+    println!("For 2,2: ");
+    live_neighbor_count(2,2);
+}
+
+fn get_index(row: u32, column: u32) -> usize {
+    (row *24 + column) as usize
+}
+
+fn live_neighbor_count(row: u32, column: u32) -> u8 {
+    let mut count = 0;
+    for delta_row in [5 - 1, 0, 1].iter().cloned() {
+        for delta_col in [5 - 1, 0, 1].iter().cloned() {
             if delta_row == 0 && delta_col == 0 {
                 continue;
             }
 
-            let neighbor_row = (0 + delta_row) % 25;
-            let neighbor_col = (2 + delta_col) % 20;
-
-            println!("Neighbors: {neighbor_row} {neighbor_col}");
+            let neighbor_row = (row + delta_row) % 5;
+            let neighbor_col = (column + delta_col) % 5;
+            let idx = get_index(neighbor_row, neighbor_col);
+            println!("Row: {neighbor_row} | Col: {neighbor_col} | Index {idx} ")
         }
-        
-    }}
+    }
+    count
+}
